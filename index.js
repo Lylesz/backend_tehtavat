3ef752781918a17cabe4dca5c7b72766e08bcba0
Mikko Uotinen <mikko.uotinen.j@gmail.com>
2024-10-10 22:09:54 +0300

    uibuild

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 
 const app = express()
 
 app.use(express.json())
 app.use(morgan('tiny')) //ottaa morganin käyttöön "tiny" -formaatissa
+app.use(cors()) //ottaa corsin käyttöön
+app.use(express.static('dist')) //frontendin näyttämiseen
 
 let persons = [
     {
@@ -98,7 +101,7 @@ let persons = [
     response.json(person)
   })
   
-  const PORT = 3001
+  const PORT = process.env.PORT || 3001
   app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
   })
